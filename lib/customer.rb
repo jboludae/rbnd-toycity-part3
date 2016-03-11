1b6edb47474f3a03fbfbf0917c04ec3875556cb3
jboludae <jboludae@gmail.com>
2016-03-11 09:44:01 +0100

    The Products class is now fully functional

---
+++ lib/customer.rb
@@ -0,0 +1,5 @@
+class Customer
+	attr_reader :name
+	def initialize(options = {})
+		@name = options[:name]
+end
--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,44 @@
 class Product
 	@@products = []
+	attr_reader :title, :price, :stock
 	def initialize(options= {})
-		@@products << self
+		@title = options[:title]
+		@price = options[:price]
+		@stock = options[:stock]
+		add_to_products(@title)
 	end
 	def self.all
 		@@products
+	end
+
+	def self.find_by_title(title)
+		@@products.each do |product|
+			if product.title == title
+				return product
+			end
+		end
+		puts "Product does not exist"
+	end
+
+	def self.in_stock
+		return @@products.select {|item| item.in_stock?}
+	end
+
+	def in_stock?
+		if @stock>0
+			return true
+		end
+		return false
+	end
+
+	private
+
+	def add_to_products(title)
+		@@products.each do |item|
+			if item.title == title
+				raise  DuplicateProductError, "#{item.title} already"
+			end
+		end
+		@@products << self
 	end
 end
